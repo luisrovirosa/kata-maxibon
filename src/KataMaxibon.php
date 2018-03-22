03e331fd89d320b3a43fdbf08c8e31b644401eb7
Luis Rovirosa <luisrovirosa@gmail.com>
2018-03-22 07:52:34 +0100

    Property based testing tool installed and test example

---
+++ src/KataMaxibon.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace LuisRovirosa;
+
+class KataMaxibon
+{
+}
---
+++ tests/KataMaxibonTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LuisRovirosa\Tests;
+
+use Eris\Generator;
+use Eris\TestTrait;
+use PHPUnit\Framework\TestCase;
+
+class KataMaxibonTest extends TestCase
+{
+    use TestTrait;
+
+    // https://github.com/giorgiosironi/eris
+
+    public function testNaturalNumbersMagnitude()
+    {
+        $this->forAll(
+            Generator\choose(0, 1000)
+        )
+             ->then(function ($number) {
+                 $this->assertTrue(
+                     $number >= 0
+                 );
+             });
+    }
+}
